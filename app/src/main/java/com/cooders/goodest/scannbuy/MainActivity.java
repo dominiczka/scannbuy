a7609aac85cc81c2c8d584affa429daf90931e13
Mateusz Rajch <matti.r@wp.pl>
2017-11-29 09:09:31 +0100

    skanowanie kodu sklepu (beta) można zamienić na lokalizację

--- app/src/main/java/com/cooders/goodest/scannbuy/MainActivity.java
+++ app/src/main/java/com/cooders/goodest/scannbuy/MainActivity.java
@@ -1,15 +1,25 @@
 package com.cooders.goodest.scannbuy;
 
+import android.content.Intent;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Button;
+import android.widget.Toast;
+
+import com.cooders.goodest.scannbuy.barcode.BarcodeCaptureActivity;
+import com.google.android.gms.common.api.CommonStatusCodes;
+import com.google.android.gms.vision.barcode.Barcode;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button buttonStartShopping;
+
+    private static final String TAG = "BarcodeMain";
+    private static final int RC_BARCODE_CAPTURE = 9001;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,14 +28,37 @@ public class MainActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+
+        buttonStartShopping = (Button) findViewById(R.id.buttonStartShopping);
+        buttonStartShopping.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                // launch barcode activity
+                Intent intent = new Intent(MainActivity.this, BarcodeCaptureActivity.class);
+                intent.putExtra(BarcodeCaptureActivity.AutoFocus, true);
+                intent.putExtra(BarcodeCaptureActivity.UseFlash, false);
+                startActivityForResult(intent, RC_BARCODE_CAPTURE);
             }
         });
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == RC_BARCODE_CAPTURE) {
+            if (resultCode == CommonStatusCodes.SUCCESS) {
+                if (data != null) {
+                    Barcode barcode = data.getParcelableExtra(BarcodeCaptureActivity.BarcodeObject);
+                    Intent intent = new Intent(MainActivity.this, ScanActivity.class);
+                    intent.putExtra("shopID", barcode.rawValue);
+                    startActivity(intent);
+                    finish();
+                }
+            } else {
+                Toast.makeText(getApplicationContext(), getString(R.string.barcode_error), Toast.LENGTH_LONG).show();
+            }
+        }
+        else {
+            super.onActivityResult(requestCode, resultCode, data);
+        }
     }
 
     @Override
---
+++ app/src/main/java/com/cooders/goodest/scannbuy/ScanActivity.java
@@ -0,0 +1,15 @@
+package com.cooders.goodest.scannbuy;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ScanActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_scan);
+
+        // key shopID i tam masz szopID xD
+    }
+}
